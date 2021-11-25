5a50ed0b66feea364e397ee491c43823ede0b6ad
Kyle Carbonell <s.carbonell.kyle@cmu.edu.ph>
2021-11-25 14:13:17 +0800

    edited chartisan library

--- resources/views/layouts/guestStyle.blade.php
+++ resources/views/layouts/guestStyle.blade.php
@@ -68,7 +68,8 @@ The above copyright notice and this permission notice shall be included in all c
     <script src="{{ asset('paper') }}/demo/jquery.sharrre.js"></script>
     
     <!-- Charting library -->
-    <script src="https://unpkg.com/chart.js@2.9.3/dist/Chart.min.js"></script>
+    <!-- <script src="https://unpkg.com/chart.js@2.9.3/dist/Chart.min.js"></script> -->
+    <script src="./node_modules/chart.js/dist/Chart.min.js"></script>
     <!-- Chartisan -->
     <script src="https://unpkg.com/@chartisan/chartjs@^2.1.0/dist/chartisan_chartjs.umd.js"></script>
     @stack('scripts')
